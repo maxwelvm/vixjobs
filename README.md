637a105e0cbbb7af36cf3fabede80b6fd81fe272
Maxwel Machado <maxwel.v.machado@gmail.com>
2015-05-16 19:59:09 +0000

    inicio do projeo vixjobs

---
+++ README.md
@@ -0,0 +1,4 @@
+vixjobs
+=======
+
+A Symfony project created on April 23, 2015, 3:33 am.
---
+++ src/MVM/CurriculoBundle/MVMCurriculoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVM\CurriculoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MVMCurriculoBundle extends Bundle
+{
+}
